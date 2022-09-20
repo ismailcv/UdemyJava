704ec8fbfbcd18144ff1dd25a46b75f9e6bd3312
İsmail Can VARLI <icanvarli@outlook.com>
2022-09-20 21:26:54 +0300

    Udemy java eğitim setinin bir parçası. Bu kısımda OOP kısmına ilk giriş ve constructor ile alakalı çalışmalar var.

---
+++ src/ClassesMetod/Test.java
@@ -0,0 +1,27 @@
+package ClassesMetod;
+
+public class Test {
+     public static void main(String[] args) {
+        
+        /*Araba araba1 = new Araba();
+        
+        
+        araba1.setModel("Renault");
+        
+        System.out.println("Arabanın modeli " + araba1.getModel());
+        
+        
+        araba1.setKapilar(-4);*/
+        
+        
+        /*Araba araba2;
+        
+        araba2.setKapilar(10);*/
+        
+        Araba araba3 = new Araba();
+        
+        araba3.setModel("Renault");
+         System.out.println(araba3.getModel());
+         
+    }
+}
